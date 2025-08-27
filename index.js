c986bdc3ce0250b5c69d96242721e03169a0f035
sunil <sunilseervik6@gmail.com>
2025-08-27 12:46:23 +0530

    basic sql connectivity with backend

---
+++ index.js
@@ -0,0 +1,42 @@
+const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'delta_app',
+  password: 'sunil@mysql',
+});
+//placeholder;
+let q = "INSERT INTO user(id, username, email, password) VALUES ? ";
+
+
+let  getRandomUser = () => {
+  return [
+    faker.string.uuid(),
+    faker.internet.username(),
+    faker.internet.email(), 
+    faker.internet.password(),
+ 
+  ];
+}
+
+let data = [];
+for(let i=1 ; i<=100; i++){
+  data.push(getRandomUser());
+}
+
+try{
+  connection.query(q, [data] , (err, results)=>{
+    if(err) throw err;
+    console.log(results);
+  });
+}
+catch(err){
+  console.log(err);
+}
+connection.end();
+
+
+ 
---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user(
+    id VARCHAR(50) PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL
+);
+ 
